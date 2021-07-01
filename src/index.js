6b92203f8bf90b12b0064b4ab29df5e5be88c852
Abdi Yusuf <abdiyuu7@gmail.com>
2021-07-01 12:14:44 -0600

    added nav and buttons

--- src/index.js
+++ src/index.js
@@ -4,7 +4,29 @@ import menuPage from './menu.js';
 import contactPage from './contact.js'
 import './style.css';
 
-let content = document.querySelector('#content');
+const content = document.querySelector('#content');
 
+function makeNav() {
+	const nav = document.createElement('nav');
+	const btns = [];
+	for(let i = 0; i <3; i++) {
+		btns.push(document.createElement('button'));
+	}
+	btns.forEach(btn => {
+		btn.classList.add('btn_nav')
+		btn.addEventListener('click', switchPage);
+		nav.appendChild(btn);
+	});
+	btns[0].innerText = 'Home';
+	btns[1].innerText = 'Menu';
+	btns[2].innerText = 'Contact';
+
+	return nav;
+}
+
+content.appendChild(makeNav());
 content.appendChild(makeHome());
 
+function switchPage(e) {
+	console.log(e.target.innerText);
+}
